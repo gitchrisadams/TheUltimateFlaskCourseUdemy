5e00de4640f04072f165b64ed67946823fa9d2f1
Christopher Adams <chrismichaeladams@gmail.com>
2025-08-19 08:24:36 -0400

    Editing of profile and saving edit is working

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from .extensions import db
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import Language, Topic, Member
 
 main = Blueprint("main", __name__)
@@ -34,29 +34,46 @@ def index(member_id):
         learn_new_interest = request.form["learn_new_interest"]
         interest_in_topics = request.form.getlist("interest_in_topics")
 
-        # Create a new member
-        member = Member(
-            email=email,
-            password=password,
-            location=location,
+        # Check if member already exists, and if so, do an edit vs new member
+        if member:
+            member.email = email
+            if password:
+                member.password = password
+            member.location = location
             # Convert first_learn_date to a date w/ a format Year-Month-Day
-            first_learn_date=datetime.strptime(first_learn_date, "%Y-%m-%d"),
-            fav_language=fav_language,
-            about=about,
-            learn_new_interest=(True if learn_new_interest == "yes" else False),
-        )
+            member.first_learn_date = datetime.strptime(first_learn_date, "%Y-%m-%d")
+            member.fav_language = fav_language
+            member.about = about
+            member.learn_new_interest = True if learn_new_interest == "yes" else False
 
-        # Add member to the DB
-        db.session.add(member)
+            # Clear out all the topics
+            member.interest_in_topics[:] = []
+        else:
+            # Create a new member
+            member = Member(
+                email=email,
+                password=password,
+                location=location,
+                # Convert first_learn_date to a date w/ a format Year-Month-Day
+                first_learn_date=datetime.strptime(first_learn_date, "%Y-%m-%d"),
+                fav_language=fav_language,
+                about=about,
+                learn_new_interest=(True if learn_new_interest == "yes" else False),
+            )
 
-        # Loop through all topics and append topic to member
+            # Add member to the DB
+            db.session.add(member)
+
+        # Loop through all topics and append topic to members
         for topic_id in interest_in_topics:
             topic = Topic.query.get(int(topic_id))
             member.interest_in_topics.append(topic)
 
         db.session.commit()
 
-        return "Form Submitted!"
+        # Redirect back to main page and use member_id if we have one so /1 /2 etc..
+        # So can edit their profile if there is one vs seeing new form.
+        return redirect(url_for("main.index", member_id=member.id))
 
     languages = Language.query.all()
     topics = Topic.query.all()
